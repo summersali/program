5b861cf00f4e25276705171b684381db33ea915a
summersali <148704986+summersali@users.noreply.github.com>
2024-12-13 11:56:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()]
 })
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath:'/summersali/'
+})
